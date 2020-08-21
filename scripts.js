7744e7bb5fcf26ac2a242bf0efa5b84a773df950
daniel-santamaria <daniel.santamaria.001@gmail.com>
2020-08-21 18:25:08 -0300

    sidenav category transitions and single selection

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const cover = document.getElementById('cover');
 const sideMenuButton = document.getElementById('menuButton');
+const sidenavCategories = document.querySelectorAll('input[type="checkbox"]');
 
 cover.addEventListener('click', function() {
     toggleSideMenu();
@@ -14,3 +15,15 @@ function toggleSideMenu() {
     document.body.classList.toggle('sidenav-active');  
     document.body.classList.toggle('noscroll');
   }
+
+window.addEventListener('load', function() {
+    for(let i = 0; i < sidenavCategories.length; i++) {
+        sidenavCategories[i].addEventListener('change', function () {
+            for(let j = 0; j < sidenavCategories.length; j++) {
+                if(sidenavCategories[i] != sidenavCategories[j]) {
+                sidenavCategories[j].checked = false;
+                }
+            }
+        })
+    }
+})
